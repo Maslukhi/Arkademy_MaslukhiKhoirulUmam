7acf9943c19153ded730213b38ef6e124b05a5ca
Maslukhi Khoirul Umam <45111050+Maslukhi@users.noreply.github.com>
2018-11-17 14:46:25 +0700

    Add files via upload

---
+++ nomor4.php
@@ -0,0 +1,14 @@
+<?php
+// Menetapkan tanggal awal, format tanggal dalam struktur bahasa Inggris. 
+$date_from = "2018-11-01";
+$date_from = strtotime($date_from); // Mengubah tanggal ke UNIX timestamp
+
+// Menetapkan tanggal akhir, tanggal tertulis dalam format struktur bahasa Inggris. 
+$date_to = "2018-11-05";
+$date_to = strtotime($date_to); // Mengubah tanggal ke sebuah UNIX timestamp
+
+// Perulangan dari tanggal awal ke akhir tanggal and keluaran semua tanggal di antara kedua tanggal tersebut 
+for ($i=$date_from; $i<=$date_to; $i+=86400) {
+    echo date("'Y-m-d'", $i).' , ';
+}
+?>
